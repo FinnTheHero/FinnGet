010e0f3bc90226403b3c2ff4919595815d36c7a9
Aleksandre <aleksandre.nozadze.1@iliauni.edu.ge>
2023-08-08 15:55:45 +0400

    First commit for the project, uploading core file and functions

---
+++ Logo.psm1
@@ -0,0 +1,27 @@
+Function Get-WindowsArt()
+{
+    [string[]] $ArtArray  =
+            '                                    ..       ',
+            '                             ..oodMMMM       ',
+            '                    ..oodMMMMMMMMMMMMM       ',
+            '        ...oodMMM  MMMMMMMMMMMMMMMMMMM       ',
+            '   oooMMMMMMMMMMM  MMMMMMMMMMMMMMMMMMM       ',
+            '   MMMMMMMMMMMMMM  MMMMMMMMMMMMMMMMMMM       ',
+            '   MMMMMMMMMMMMMM  MMMMMMMMMMMMMMMMMMM       ',
+            '   MMMMMMMMMMMMMM  MMMMMMMMMMMMMMMMMMM       ',
+            '   MMMMMMMMMMMMMM  MMMMMMMMMMMMMMMMMMM       ',
+            '   MMMMMMMMMMMMMM  MMMMMMMMMMMMMMMMMMM       ',
+            '                                             ',
+            '   MMMMMMMMMMMMMM  MMMMMMMMMMMMMMMMMMM       ',
+            '   MMMMMMMMMMMMMM  MMMMMMMMMMMMMMMMMMM       ',
+            '   MMMMMMMMMMMMMM  MMMMMMMMMMMMMMMMMMM       ',
+            '   MMMMMMMMMMMMMM  MMMMMMMMMMMMMMMMMMM       ',
+            '   MMMMMMMMMMMMMM  MMMMMMMMMMMMMMMMMMM       ',
+            '   ^^^MMMMMMMMMMM  MMMMMMMMMMMMMMMMMMM       ',
+            '          ```^^^^  MMMMMMMMMMMMMMMMMMM       ',
+            '                        ```^^^^^^MMMMM       ',
+            '                                             ',
+            '                                             ';
+    
+    return $ArtArray;
+}
---
+++ finnget.psm1
@@ -0,0 +1,44 @@
+#### Screenfetch for powershell
+#### Author Julian Chow
+
+
+Function finnget($distro)
+{
+    $AsciiArt = . Get-WindowsArt;
+
+    $SystemInfoCollection = . Get-SystemSpecifications;
+    $LineToTitleMappings = . Get-LineToTitleMappings;
+
+    if ($SystemInfoCollection.Count -gt $AsciiArt.Count) 
+    { 
+        Write-Error "System Specs occupies more lines than the Ascii Art resource"
+    }
+
+    for ($line = 0; $line -lt $AsciiArt.Count; $line++) 
+    {
+        Write-Host $AsciiArt[$line] -f Blue -NoNewline;
+        Write-Host $LineToTitleMappings[$line] -f Red -NoNewline;
+
+        if ($line -eq 0) 
+        {
+            Write-Host $SystemInfoCollection[$line] -f Red;
+        }
+
+        elseif ($SystemInfoCollection[$line] -like '*:*') 
+        {
+            $Seperator = ":";
+            $Splitted = $SystemInfoCollection[$line].Split($seperator);
+
+            $Title = $Splitted[0] + $Seperator;
+            $Content = $Splitted[1];
+
+            Write-Host $Title -f Red -NoNewline;
+            Write-Host $Content;
+        }
+        else 
+        {
+            Write-Host $SystemInfoCollection[$line];            
+        }
+    }
+}
+
